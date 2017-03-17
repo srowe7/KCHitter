033abfdb965da63095b4f1ca833bf0b0c89fcbf5
sdavi113 <sdavi113@stumail.jccc.edu>
2017-03-16 20:23:26 -0500

    Add files via upload

---
+++ Models/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KCHitterApp.Models
+{
+    public class Admin
+    {
+
+        public int AdminID { get; set; }
+
+        public String AdminName { get; set; }
+
+        public virtual ICollection<Player> Players { get; set; }
+
+    }
+}
