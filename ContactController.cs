3c089869864640cedcc873a30715b5cab53ea426
MetieA <medessa@stumail.jccc.edu>
2017-04-08 01:14:35 -0500

    Contact form
    Created contact form

---
+++ ContactController.cs
@@ -0,0 +1,42 @@
+﻿using KCHitterDAD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KCHitterDAD.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new ContactForm());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<PartialViewResult> Submit(ContactForm model)
+        {
+            bool isMessageSent = true;
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await EmailService.SendContactForm(model);
+                }
+                catch (Exception ex)
+                {
+                    isMessageSent = false;
+                }
+            }
+            else
+            {
+                isMessageSent = false;
+            }
+            return PartialView("_SubmitMessage", isMessageSent);
+        }
+    }
+}
